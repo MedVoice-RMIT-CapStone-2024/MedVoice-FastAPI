39d38dab6625ed95ae75631e7c2c5b403efdc03b
dolelongan <dolelongan@gmail.com>
2024-07-24 16:03:55 +0700

    MVC-120 Update docker compose for ollama on local

--- app/config/google_project_config.py
+++ app/config/google_project_config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 # Access environment variables
 cloud_details = {
-    "project_id": os.getenv("PROJECT_ID"),
-    "bucket_name": os.getenv("BUCKET_NAME")
+    "project_id": os.getenv("GCLOUD_PROJECT_ID"),
+    "bucket_name": os.getenv("GCLOUD_STORAGE_BUCKET")
 }
