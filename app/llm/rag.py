29b9db5cebc1cfdaccb8b2c3427b57c7c44a4dee
dolelongan <dolelongan@gmail.com>
2024-08-02 21:30:10 +0700

    Change base url for ollama

--- app/llm/rag.py
+++ app/llm/rag.py
@@ -3,41 +3,50 @@ from langchain_core.prompts import PromptTemplate
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_community.document_loaders import PyPDFLoader, JSONLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-from langchain import hub 
+from langchain_community.vectorstores import PGVector
+from langchain import hub
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.embeddings import OllamaEmbeddings
 from difflib import SequenceMatcher
-
 import time, asyncio
 
-from .replicate_models import init_replicate, llamaguard_evaluate_safety
+from .replicate_models import init_replicate 
+from .replicate_models import llamaguard_evaluate_safety
+from ..core.db_config import settings
+
+# async def llamaguard_evaluate_safety(question):
+#     return "safe"
 
 class BaseRAGSystem:
     def __init__(self):
         self.llm = init_replicate()
         self.rag_chain = None
+        self.vectorstore = None
         self.conversation_state = {}
 
-    async def query_model(self, question):
+    async def query_model(self, question, streaming=False):
         if self.rag_chain is None:
             return "No documents have been indexed yet."
 
-        # if await llamaguard_evaluate_safety(question) == " unsafe":
-        #     return "Sorry, I cannot answer this question, please try again"
-        # else:
         start_time = time.perf_counter()
-        answer = self.rag_chain.invoke(question)
+        answer = ""
+
+        if streaming:
+            async for token in self.async_token_stream(question):
+                self.token_callback(token)
+                answer += token + " "
+        else:
+            answer = self.rag_chain.invoke(question)
+
         end_time = time.perf_counter()
         print(f"\nRaw output runtime: {end_time - start_time} seconds\n")
-        return answer
+        return answer.strip()
 
     def similar(self, a, b):
         return SequenceMatcher(None, a, b).ratio()
 
-    async def handle_question(self, question):
-        # Check for a similar question
+    async def handle_question(self, question, streaming=False):
         similar_question = None
         for prev_question in self.conversation_state:
             if self.similar(prev_question, question) > 0.8:
@@ -47,9 +56,18 @@ class BaseRAGSystem:
         if similar_question:
             return self.conversation_state[similar_question]
         else:
-            answer = await self.query_model(question)
+            answer = await self.query_model(question, streaming=streaming)
             self.conversation_state[question] = answer
             return answer
+
+    async def async_token_stream(self, question: str):
+        response = self.rag_chain.invoke(question)
+        for token in response.split():
+            yield token
+            await asyncio.sleep(0.01)
+
+    def token_callback(self, token):
+        print(token, end=' ', flush=True)
 
 class RAGSystem_PDF(BaseRAGSystem):
     def __init__(self, file_path):
@@ -57,11 +75,9 @@ class RAGSystem_PDF(BaseRAGSystem):
         self.index_pdf(file_path)
 
     def index_pdf(self, file_path):
-        # Indexing: Load
         loader = PyPDFLoader(file_path)
         docs = loader.load()
 
-        # Indexing: Split
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
             chunk_overlap=200,
@@ -69,18 +85,23 @@ class RAGSystem_PDF(BaseRAGSystem):
         )
         all_splits = text_splitter.split_documents(docs)
 
-        # Indexing: Store
-        embedding = OllamaEmbeddings(model="nomic-embed-text")
-        vectorstore = Chroma.from_documents(
+        embedding = OllamaEmbeddings(base_url="http://ollama:11434", model="nomic-embed-text")
+
+        CONNECTION_STRING = settings.DATABASE_URL
+        COLLECTION_NAME = 'embeddings.pdf_documents'
+
+        self.vectorstore = PGVector.from_documents(
             documents=all_splits,
             embedding=embedding,
+            collection_name=COLLECTION_NAME,
+            connection_string=CONNECTION_STRING,
         )
-        retriever = vectorstore.as_retriever(
+
+        retriever = self.vectorstore.as_retriever(
             search_type="similarity",
             search_kwargs={"k": 6},
         )
 
-        # Store retriever for later use
         prompt = hub.pull("rlm/rag-prompt")
 
         def format_docs(docs):
@@ -90,46 +111,62 @@ class RAGSystem_PDF(BaseRAGSystem):
             return (
                 {"context": retriever | format_docs, "question": RunnablePassthrough()}
                 | prompt
-                | self.llm 
+                | self.llm
                 | StrOutputParser()
             )
 
         self.rag_chain = create_rag_chain()
-        
+
         return {"message": "PDF indexed successfully"}
 
 class RAGSystem_JSON(BaseRAGSystem):
     def __init__(self, file_path):
         super().__init__()
         self.index_json(file_path)
-    
+
     def index_json(self, file_path):
         loader = JSONLoader(file_path, jq_schema=".prizes[]", text_content=False)
         docs = loader.load()
 
-        # Indexing: Store
-        embedding = OllamaEmbeddings(model="nomic-embed-text")
-        vectorstore = Chroma.from_documents(
+        embedding = OllamaEmbeddings(base_url="http://ollama:11434", model="nomic-embed-text")
+
+        CONNECTION_STRING = settings.DATABASE_URL
+        COLLECTION_NAME = 'embeddings.json_documents'
+
+        self.vectorstore = PGVector.from_documents(
             documents=docs,
             embedding=embedding,
+            collection_name=COLLECTION_NAME,
+            connection_string=CONNECTION_STRING,
         )
-        retriever = vectorstore.as_retriever(
+
+        retriever = self.vectorstore.as_retriever(
             search_type="similarity",
             search_kwargs={"k": 6},
         )
 
-        # Store retriever for later use
         prompt = hub.pull("rlm/rag-prompt")
 
         def create_rag_chain():
             return (
                 {"context": retriever, "question": RunnablePassthrough()}
                 | prompt
-                | self.llm 
+                | self.llm
                 | StrOutputParser()
             )
 
         self.rag_chain = create_rag_chain()
-        
+
         return {"message": "JSON File indexed successfully"}
     
+# async def main():
+#     chatbot = RAGSystem_JSON("sample-data.json")
+#     print(chatbot.index_json("sample-data.json"))
+
+#     # Mock query
+#     question = "Who won the chemistry prize in 2021?"
+#     answer = await chatbot.handle_question(question)
+#     print(f"Question: {question}\nAnswer: {answer}")
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
