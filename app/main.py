e8e3d264f7fac0d643dc3a4b6122be105fe4fcb7
dolelongan <dolelongan@gmail.com>
2024-08-09 11:03:02 +0700

    MVC-131 fix db config

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ from .db.init_db import initialize_all_databases
 
 # Change the value for the local development
 ON_LOCALHOST = 0
-RAG_SYS = 0
+RAG_SYS = 1
 # Determine if running in Docker
 running_in_docker = os.getenv('RUNNING_IN_DOCKER', 'false') == 'true'
 
