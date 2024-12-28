098b2805f62cbc7d565e74613dd82db4b60877f4
dolelongan <dolelongan@gmail.com>
2024-12-28 10:27:51 +0700

    chore: rmove patients db for later

--- app/utils/json_helpers.py
+++ app/utils/json_helpers.py
@@ -2,12 +2,6 @@ import json
 from pygments import highlight, lexers, formatters
 from sqlalchemy.ext.asyncio import AsyncSession
 from ..schemas.patient import PatientCreate
-from ..crud.crud_patient import create_patient
-
-async def json_to_sql(db: AsyncSession, patient_json: str):
-    patient_dict = json.loads(patient_json)
-    patient_create = PatientCreate(**patient_dict)
-    return await create_patient(db, patient_create)
 
 def pretty_print_json(data):
     formatted_json = json.dumps(data, sort_keys=True, indent=4)
